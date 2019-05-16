184170f23e2782844ba5e7468e38fdb43e0cf261
yohanhermawan <han18hsw@gmail.com>
2019-05-16 12:36:11 +0700

    up

--- subscribe.php
+++ subscribe.php
@@ -7,15 +7,14 @@ define( "RECIPIENT_EMAIL", "info@bumi-international.com" );
 
 // Read the form values
 $success = false;
-$senderName = isset( $_POST['username'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['username'] ) : "";
 $senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
 $message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
 
 // If all values exist, send the email
-if ( $senderName && $senderEmail && $message) {
+if ($senderEmail && $message) {
   $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
-  $headers = "From: " . $senderName . " <" . $senderEmail . ">";
-  $msgBody = " Message: " . $message . "";
+  $headers = "From: " . " <" . $senderEmail . ">";
+  $msgBody = " Subscribe";
   $success = mail( $recipient, $headers, $msgBody );
 
   //Set Location After Successsfull Submission
